73f09baf96ddf22fb48b43b8c07a9cb20361847d
Enmanuel <enmareco.co@gmail.com>
2025-01-03 18:22:42 -0300

    change .js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.static(path.join(__dirname, "src/build")));
 app.use("/", router);
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "src/build", "index.html"));
+  res.sendFile(path.join(__dirname, "src/build", "index.js"));
 });
 
 app.listen(PORT, () => console.log("Server Running"));

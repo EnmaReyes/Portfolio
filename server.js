34844340aaf5bd680242132679bdcd4fd0759ff2
Enmanuel <enmareco.co@gmail.com>
2025-01-03 19:55:32 -0300

    change directory index.html

--- server.js
+++ server.js
@@ -10,12 +10,12 @@ const PORT = process.env.PORT || 4002;
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "src/build")));
-
 app.use("/", router);
 
+app.use(express.static(path.join(__dirname, "build")));
+
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "src/build", "index.html"));
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 app.listen(PORT, () => console.log("Server Running"));
